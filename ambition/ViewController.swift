b65cc2ea092e08ab0147020d0c5489e483187d3c
amitchaulagain <achaulagain123@gmail.com>
2022-03-28 07:01:16 +0545

    first ios app

---
+++ ambition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ambition
+//
+//  Created by Admin on 3/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+
+}
+
